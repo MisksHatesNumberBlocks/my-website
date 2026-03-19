021690b1ef8ba06c36abcc54f0f7a08cb499593c
levi the karrot <mb3096767@gmail.com>
2026-03-19 14:07:10 -0300

    remove bruh mode

--- detect.js
+++ detect.js
@@ -1,7 +1,4 @@
 var mainetanceMode = true;
-var bruhMode = false;
 if (mainetanceMode) {
   window.location.replace('./mainetanceMode.html');
-} else if (bruhMode) {
-  window.location.replace('./huh.html');
 }
