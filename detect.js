c1b2fe453e6b2e92c741e1f1e6fbefa75c549772
Smartass Samuel <mb3096767@gmail.com>
2026-06-18 10:19:13 -0300

    Add warning prompt for accessing old website

--- detect.js
+++ detect.js
@@ -1,1 +1,6 @@
-window.location.replace('redirect.html')
+let warning = prompt("This website is old and not updated. Are you sure you want to access this page?")
+if (warning == "y" || warning == "yes") {
+  alert("You have been warned.");
+} else {
+  location.replace("//miskshatesnumberblocks.github.io");
+}
